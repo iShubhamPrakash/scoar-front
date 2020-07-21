0da9f69d5531de5331c397c4c65ef7d5877d3f2d
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-07-21 05:49:32 +0530

    New login API implemented

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,21 +1,59 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  signIn,
+  signOut
+} from "../../store/actions/authActions";
+
+const checkUserExistURL= 'https://score-backend.herokuapp.com/scoar/cred/checkuserexists/'
+const sendOTPURL= 'https://score-backend.herokuapp.com/scoar/auth/sendotp/'
+const verifyOTPURL ='https://score-backend.herokuapp.com/scoar/auth/verifyotp/'
 
 export default function Login() {
   const [mobile, setMobile] = useState("");
   const [otp, setOTP] = useState("");
   const [next, setNext] = useState(false);
+  const [userData, setUserData] = useState({})
   let history = useHistory();
+  const dispatch = useDispatch();
+
+
+  const sendOTP = async (mobile)=>{
+    try{
+    	const res = await fetch(`${sendOTPURL}${mobile}`)
+    	const result = await res.text()
+      console.log("result:",result)
+    	if(result === '"SUCCESS"'){
+			  setNext(true);
+    	}else{
+    		alert("Try again! OTP could not be sent!!")
+    	}
+    }catch(e){
+      console.log("Error sending OTP",e)
+    	alert("Try again! Something went wrong!!")
+    }
+  }
+
 
   const handleNext = async () => {
     // setNext(true);
 
     try{
-    	const res = await fetch(`https://score-backend.herokuapp.com/scoar/auth/sendotp/${mobile}`)
-
+    	let res = await fetch(`${checkUserExistURL}${mobile}`)
+  
     	if(res.status === 200){
-    		setNext(true)
+        res = await res.json()
+        console.log("sucess", res)
+        const {contactNo, role, uid} = res.credential;
+        if(uid !== 0){
+          setUserData({contactNo, role: role.toLowerCase(), uid})
+          sendOTP(mobile)
+        }else{
+          alert("User does not exist, Please sign up...")
+        }
     	}else{
     		alert("Try again! Something went wrong!!")
     	}
@@ -28,17 +66,19 @@ export default function Login() {
     // alert("Success");
 
     try{
-    	const res = await fetch(`https://score-backend.herokuapp.com/scoar/auth/verifyotp/${otp}`)
+    	const res = await fetch(`${verifyOTPURL}${otp}`)
     	const result = await res.text()
 
     	if(result === 'approved'){
-			console.log("Success")
-			history.push("/whiteboard");
+        console.log("Login Success")
+        dispatch(signIn(userData))
+			  history.push("/whiteboard");
     	}else{
-    		alert("Try again! Something went wrong!!")
+    		alert("Invalid OTP!!")
     	}
 
     }catch(e){
+      console.log("Error verifying OTP", e)
     	alert("Try again! Something went wrong!!")
     }
   };
