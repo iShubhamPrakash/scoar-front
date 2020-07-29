5cf8059156ffc7f843dd1d029091a074875c2490
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-07-29 10:43:57 +0530

    Student list table ready

--- src/components/Dashboard/Classroom/ClassroomView.js
+++ src/components/Dashboard/Classroom/ClassroomView.js
@@ -5,6 +5,8 @@ import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import SubjectIcon from "@material-ui/icons/Subject";
 import ScheduleIcon from "@material-ui/icons/Schedule";
 import EditIcon from "@material-ui/icons/Edit";
+import Typography from "@material-ui/core/Typography";
+import Checkbox from "@material-ui/core/Checkbox";
 
 export default function ClassroomView(props) {
 	const history = useHistory();
@@ -76,8 +78,8 @@ const ClassDetailCard = (props) => {
 			<div className="top">
 				<ClassData {...props} />
 			</div>
-      <div className="bottom">
-				<ClassData {...props} />
+			<div className="bottom">
+				<StudentList {...props} />
 			</div>
 		</div>
 	);
@@ -172,15 +174,150 @@ const ClassData = (props) => {
 					</div>
 				</div>
 			</div>
-      <h5>Description</h5>
-      <div className="description">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi,
-          nobis natus debitis hic asperiores minima!
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi,
-          nobis natus debitis hic asperiores minima!
-        </p>
-      </div>
+			<h5>Description</h5>
+			<div className="description">
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi,
+					nobis natus debitis hic asperiores minima! Lorem ipsum, dolor sit amet
+					consectetur adipisicing elit. Excepturi, nobis natus debitis hic
+					asperiores minima!
+				</p>
+			</div>
+		</div>
+	);
+};
+
+const StudentList = (props) => {
+	const history = useHistory();
+	return (
+		<div className="studentList">
+			<div className="table container-flex">
+				<div className="header row">
+					<div className="col col-1 col-sm-1 col-md-1 col-lg-1">
+						<Checkbox
+							color="primary"
+							inputProps={{ "aria-label": "secondary checkbox" }}
+							size="small"
+							className="totalCheck"
+						/>
+					</div>
+					<div className="col col-3 col-sm-3 col-md-3 col-lg-3">
+						<p>Name</p>
+					</div>
+					<div className="col col-2 col-sm-2 col-md-2 col-lg-2">
+						<p>Join Date</p>
+					</div>
+					<div className="col col-2 col-sm-2 col-md-2 col-lg-2">
+						<p>Assignment</p>
+					</div>
+					<div className="col col-2 col-sm-2 col-md-2 col-lg-2">
+						<p>Fee</p>
+					</div>
+
+					<div className="col col-1 col-sm-1 col-md-1 col-lg-1">
+						<p>Details</p>
+					</div>
+				</div>
+				<div className="body">
+					<TableRow
+						name="Shubham"
+						avatar="/shubham.png"
+						assignment="Issued"
+						fee="Paid"
+						date="23 Aug, 2020"
+						handleView={(e) => alert("View open")}
+					/>
+					<TableRow
+						name="Shubham"
+						avatar="/shubham.png"
+						assignment="Issued"
+						fee="Paid"
+						date="23 Aug, 2020"
+						handleView={(e) => alert("View open")}
+					/>
+					<TableRow
+						name="Shubham"
+						avatar="/shubham.png"
+						assignment="Issued"
+						fee="Paid"
+						date="23 Aug, 2020"
+						handleView={(e) => alert("View open")}
+					/>
+					<TableRow
+						name="Shubham"
+						avatar="/shubham.png"
+						assignment="Issued"
+						fee="Paid"
+						date="23 Aug, 2020"
+						handleView={(e) => alert("View open")}
+					/>
+					<TableRow
+						name="Shubham"
+						avatar="/shubham.png"
+						assignment="Issued"
+						fee="Paid"
+						date="23 Aug, 2020"
+						handleView={(e) => alert("View open")}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const TableRow = (props) => {
+	const { name, avatar, assignment, fee, date, handleView } = props;
+	return (
+		<div className="row">
+			<div className="col col-1 col-sm-1 col-md-1 col-lg-1">
+				<Checkbox
+					color="primary"
+					inputProps={{ "aria-label": "secondary checkbox" }}
+					size="small"
+				/>
+			</div>
+
+			<div className="col col-3 col-sm-3 col-md-3 col-lg-3">
+				<Avatar src={avatar} />
+				<Typography
+					gutterBottom
+					variant="subtitle2"
+					component="p"
+					color="textSecondary"
+					className="name"
+				>
+					{name}
+				</Typography>
+			</div>
+
+			<div className="col col-2 col-sm-2 col-md-2 col-lg-2">
+				<Typography gutterBottom variant="subtitle2" component="p">
+					{date}
+				</Typography>
+			</div>
+
+			<div className="col col-2 col-sm-2 col-md-2 col-lg-2">
+				<Typography gutterBottom variant="subtitle2" component="p">
+					{assignment}
+				</Typography>
+			</div>
+
+			<div className="col col-2 col-sm-2 col-md-2 col-lg-2">
+				<Typography
+					gutterBottom
+					variant="subtitle2"
+					component="p"
+					color="textSecondary"
+				>
+					{fee}
+				</Typography>
+			</div>
+
+			<div className="col col-1 col-sm-1 col-md-1 col-lg-1">
+				<Button size="small" onClick={(e) => handleView(e)}>
+					View
+				</Button>
+			</div>
 		</div>
 	);
 };
