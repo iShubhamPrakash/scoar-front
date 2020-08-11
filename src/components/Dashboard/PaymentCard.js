8d93ba8976ba617b3ee292fd691e97e01e7dfd40
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-08-11 10:21:23 +0530

    Payment information implemented

--- src/components/Dashboard/PaymentCard.js
+++ src/components/Dashboard/PaymentCard.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
@@ -6,42 +6,59 @@ import Button from "@material-ui/core/Button";
 import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
-import MoneyInfo from './Payment/MoneyInfo'
+import MoneyInfo from "./Payment/MoneyInfo";
 import { useSelector } from "react-redux";
 import { VIEW_PAYMENT_LIST_API_URL } from "../../constants/api";
-
+import LoadingIcon from "../UI/LoadingIcon";
 
 export default function PaymentCard() {
+	const [loading, setLoading] = useState(true);
+	const [currentList, setCurrentList] = useState([]);
+	const [receivedList, setReceivedList] = useState([]);
+	const [dueList, setDueList] = useState([]);
+	const [upcomingList, setUpcomingList] = useState([]);
 
-	const [loading, setLoading] = useState(true)
-	const [totalPaymentList, setTotalPaymentList] = useState([])
-	const [receivedList, setReceivedList] = useState([])
-	const [dueList, setDueList] = useState([])
-	const [upcomingList, setUpcomingList] = useState([])
+	const [view, setView] = useState("received"); //received - due - upcoming
 
-	const [view, setView] = useState("received") //received - due - upcoming
-
-
-	const auth = useSelector(state => state.auth)
+	const auth = useSelector((state) => state.auth);
 
 	useEffect(() => {
 		fetchPaymentList();
-	}, [])
+	}, []);
 
-	const fetchPaymentList= async ()=>{
-		try{
-			const res= await fetch(`${VIEW_PAYMENT_LIST_API_URL}${auth.token}`)
-			const data = await res.json()
-			console.log("Payment data", data)
-		}catch (e){
-			console.log("Error fetching payment list", e)
+	useEffect(() => {
+		fetchPaymentList();
+		switch (view) {
+			case "received":
+				setCurrentList(receivedList);
+				break;
+			case "due":
+				setCurrentList(dueList);
+				break;
+			case "upcoming":
+				setCurrentList(upcomingList);
+				break;
+
+			default:
+				setCurrentList([]);
 		}
-	}
+	}, [view]);
 
-
-
-
-
+	const fetchPaymentList = async () => {
+		try {
+			setLoading(true);
+			const res = await fetch(`${VIEW_PAYMENT_LIST_API_URL}${auth.token}`);
+			const data = await res.json();
+			console.log("Payment data", data);
+			setReceivedList(data.filter((item) => item.paymentStaus === "Paid"));
+			setDueList(data.filter((item) => item.paymentStaus === "NotPaid"));
+			setUpcomingList(data.filter((item) => item.paymentStaus === "Upcoming"));
+			setLoading(false);
+		} catch (e) {
+			console.log("Error fetching payment list", e);
+			setLoading(false);
+		}
+	};
 
 	return (
 		<Card className="card">
@@ -54,35 +71,59 @@ export default function PaymentCard() {
 			<CardContent className="cardContent">
 				<div className="header row">
 					<div className="moneyCard">
-						<Button variant="outlined" size="small">
+						<Button
+							variant="outlined"
+							size="small"
+							onClick={(e) => setView("received")}
+							className={view === "received" ? "active" : null}
+						>
 							<MoneyInfo balance={370} percentagePaid={70} type="Received" />
 						</Button>
 					</div>
 					<div className="moneyCard">
-						<Button variant="outlined" size="small">
+						<Button
+							variant="outlined"
+							size="small"
+							onClick={(e) => setView("due")}
+							className={view === "due" ? "active" : null}
+						>
 							<MoneyInfo balance={370} percentagePaid={70} type="Due" />
 						</Button>
 					</div>
 					<div className="moneyCard">
-						<Button variant="outlined" size="small">
+						<Button
+							variant="outlined"
+							size="small"
+							onClick={(e) => setView("upcoming")}
+							className={view === "upcoming" ? "active" : null}
+						>
 							<MoneyInfo balance={370} percentagePaid={70} type="Upcoming" />
 						</Button>
 					</div>
 				</div>
 
-				{[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((i) => (
-					<PaymentPeople
-						time={"08:34 AM"}
-						date={"23 AUG 2020"}
-						avatar={"./shubham.png"}
-						name={"Shubham Prakash"}
-						bio={"Student of CS"}
-						handleThanks={(e) => alert("Thanks sent")}
-					/>
-				))}
+				{loading ? (
+					<LoadingIcon />
+				) : currentList.length ? (
+					currentList.map((item) => (
+						<PaymentPeople
+							time={"08:34 AM"}
+							date={item.date}
+							avatar={item.profilepic}
+							name={item.name}
+							bio={"Student of CS"}
+							handleThanks={(e) => alert("Thanks sent")}
+						/>
+					))
+				) : (
+					<p className="center-text"> No data to show </p>
+				)}
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
 
-				<br />
-				<br />
 			</CardContent>
 		</Card>
 	);
@@ -93,14 +134,13 @@ const PaymentPeople = ({ time, date, avatar, name, bio, handleThanks }) => {
 		<div className="paymentPeople row">
 			<div className="col col-5 col-sm-5 col-md-5 paymentPeople__left">
 				<div className="dateData">
-					<p>{time}</p>
-					<p>{date}</p>
+					<p>{date} &nbsp; &nbsp;</p>
 				</div>
 				<Avatar alt={name} src={avatar} />
 			</div>
 			<div className="col col-7 col-sm-7 col-md-7 paymentPeople__center">
 				<p className="orange">{name}</p>
-				<p>{bio}</p>
+				{/* <p>{bio}</p> */}
 			</div>
 			{/* <div className="col col-3 col-sm-3 col-md-3 paymentPeople__right">
 				<Button
